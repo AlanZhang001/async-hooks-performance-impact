60bdbbef2f99c499c84cb840439bc70ea96e9c96
alanzhang <alnzhang@futunn.com>
2020-04-07 17:07:53 +0800

    node10

--- .history/async-hook-full_20200407142239.js
+++
@@ -1,9 +0,0 @@
-const async_hooks = require('async_hooks');
-const hook = async_hooks.createHook({
-    init(asyncId, type, triggerAsyncId, resource) { },
-    before(asyncId) { },
-    after(asyncId) { },
-    destroy(asyncId) { },
-    promiseResolve(asyncId) { },
-});
-hook.enable();
